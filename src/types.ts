343b68d87cd5973192f726547274dfce6cdec4ee
Kyle Chapman <kyleichapman@gmail.com>
2024-10-28 16:39:14 +0100

    chore: use single quotes everywhere

--- src/types.ts
+++ src/types.ts
@@ -24,7 +24,7 @@ export interface IExperience {
   startDate: string
   endDate: string
   title: string
-  type: "Full-time" | "Part-time" | "Internship"
+  type: 'Full-time' | 'Part-time' | 'Internship'
   company: string
   imagePath: string
   description: string
