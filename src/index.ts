5ad03d4783409a22080913114ddde4d210892f08
Kyle Chapman <kyleichapman@gmail.com>
2024-10-28 21:59:02 +0100

    chore: use CORS to block external requests

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import { prettyJSON } from 'hono/pretty-json'
 
 import api from './v2'
@@ -10,6 +11,14 @@ app.notFound((c) => c.json({ message: 'Not Found', ok: false }, 404))
 
 // Middleware
 app.use(prettyJSON())
+app.use('/*', cors({
+  origin: (origin, c) => {
+    return origin.endsWith('kylechapman.dev')
+      ? origin
+      : 'https://kylechapman.dev/'
+  },
+  allowMethods: ['GET']
+}))
 
 // Route with new API
 app.route('/v2', api)
