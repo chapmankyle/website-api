56d80e06dc6ab96e8b2de1931b963e43491afbae
Kyle Chapman <kyleichapman@gmail.com>
2024-10-29 08:40:59 +0100

    feat: add CSRF protection

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,32 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { csrf } from 'hono/csrf'
 import { prettyJSON } from 'hono/pretty-json'
 
 import api from './v2'
+
+// Only host that is allowed access to this API
+const ALLOWED_HOST = 'kylechapman.dev'
 
 // Create app
 const app = new Hono()
 app.get('/', (c) => c.text('Welcome!'))
 app.notFound((c) => c.json({ message: 'Not Found', ok: false }, 404))
 
-// Middleware
 app.use(prettyJSON())
+
+// Only allow requests from my domain
 app.use('/*', cors({
   origin: (origin, c) => {
-    return origin.endsWith('kylechapman.dev')
+    return origin.endsWith(ALLOWED_HOST)
       ? origin
-      : 'https://kylechapman.dev/'
+      : `https://${ALLOWED_HOST}`
   },
   allowMethods: ['GET']
 }))
+
+// CSRF protection
+app.use(csrf({ origin: ALLOWED_HOST }))
 
 // Route with new API
 app.route('/v2', api)
